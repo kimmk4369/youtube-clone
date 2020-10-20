7c5ce9a746b5478947066d044f394ec62155a699
kimmk4369 <killer5634@naver.com>
2020-10-20 21:52:34 +0900

    completed homeController

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { deleteVideo, editVideo, videoDetail, videos } from "../controllers/videoController";
+import { deleteVideo, editVideo, videoDetail, videos, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
