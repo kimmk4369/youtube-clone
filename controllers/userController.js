2dc4ffa1e6df5a4d2623d47afc004610cd5b1a74
kimmk4369 <killer5634@naver.com>
2020-10-23 22:45:12 +0900

    done pug!!!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
-}
+};
 
 export const postJoin = (req, res) => {
     const {
@@ -14,11 +16,17 @@ export const postJoin = (req, res) => {
         // To Do: Log user in
         res.redirect(routes.home);
     }
-}
+};
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
 
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+    // To Do : Process Log out
+    res.redirect(routes.home);
+};
 
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { deleteVideo, editVideo, videoDetail, videos, upload } from "../controllers/videoController";
+import { deleteVideo, editVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
