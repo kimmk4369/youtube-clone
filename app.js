1dc294bda439d3f8054adb6af8370dd4bd3004db
kimmk4369 <killer5634@naver.com>
2020-10-25 17:09:50 +0900

    Video Upload/Update/Delete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { localsMiddleware } from "./middelwares";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,23 @@
 import express from "express";
 import routes from "../routes";
-import { deleteVideo, editVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
+import { deleteVideo, getEditVideo, postEditVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// upload
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Vidoe
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 
 export default videoRouter;
